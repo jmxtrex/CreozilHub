8afec17c952f2855eb9a25d611af390509fe8a5d
jmxtrex <121318872+jmxtrex@users.noreply.github.com>
2022-12-29 16:19:16 +0300

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -1,61 +1,61 @@
-aWYgZ2FtZS5QbGFjZUlkID09IDY0OTAwMTYxOTggdGhlbg
-bG9jYWwgT3Jpb25MaWIgPSBsb2Fkc3RyaW5nKGdhbWU6SHR0cEdldCgoJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9zaGxleHdhcmUvT3Jpb24vbWFpbi9zb3VyY2UnKSkpKCk
-bG9jYWwgV2luZG93ID0gT3Jpb25MaWI6TWFrZVdpbmRvdyh7TmFtZSA9ICJVcERBVEXwn6eKU2xheWVyIFR5Y29vbiAodjEuNSkgfCBDcmVvemlsSHViIiwgSGlkZVByZW1pdW0gPSBmYWxzZSwgSW50cm9UZXh0ID0gIkNyZW96aWxIdWIiLCBTYXZlQ29uZmlnID0gdHJ1ZSwgQ29uZmlnRm9sZGVyID0gIkNyZW96aWxIdWIifSk
-ICA
-LS1WYWx1ZQ
-ICA
-ICA
-LS1GdW5jdGlvbnM
-ICA
-ICA
-LS1UYWJz
-bG9jYWwgTWFpblRhYiA9IFdpbmRvdzpNYWtlVGFiKHs
-ICBOYW1lID0gIk1haW4iLA
-ICBJY29uID0gInJieGFzc2V0aWQ6Ly80NDgzMzQ1OTk4Iiw
-ICBQcmVtaXVtT25seSA9IGZhbHNl
-fSk
-ICA
-LS1Ub2dnbGVzICA
-TWFpblRhYjpBZGRTbGlkZXIoew
-ICBOYW1lID0gIlNwZWVkSGFjayIs
-ICBNaW4gPSAwLA
-ICBNYXggPSAyMDAs
-ICBEZWZhdWx0ID0gMTYs
-ICBDb2xvciA9IENvbG9yMy5mcm9tUkdCKDI1NSwyNTUsMjU1KSw
-ICBJbmNyZW1lbnQgPSAxLA
-ICBWYWx1ZU5hbWUgPSAiIiw
-ICBDYWxsYmFjayA9IGZ1bmN0aW9uKHMpICA
-X0cuSGFja2VkV2Fsa1NwZWVkID0gcw
-ICA
-bG9jYWwgUGxycyA9IGdhbWU6R2V0U2VydmljZSgiUGxheWVycyIp
-ICA
-bG9jYWwgTXlQbHIgPSBQbHJzLkxvY2FsUGxheWVy
-bG9jYWwgTXlDaGFyID0gTXlQbHIuQ2hhcmFjdGVy
-ICA
-aWYgTXlDaGFyIHRoZW4
-bG9jYWwgSHVtID0gTXlDaGFyLkh1bWFub2lk
-SHVtLkNoYW5nZWQ6Y29ubmVjdChmdW5jdGlvbihzKQ
-SHVtLldhbGtTcGVlZCA9IF9HLkhhY2tlZFdhbGtTcGVlZA
-ZW5kKQ
-SHVtLldhbGtTcGVlZCA9IF9HLkhhY2tlZFdhbGtTcGVlZA
-ZW5k
-ICA
-ICA
-TXlQbHIuQ2hhcmFjdGVyQWRkZWQ6Y29ubmVjdChmdW5jdGlvbihDaGFyKQ
-TXlDaGFyID0gQ2hhcg
-cmVwZWF0IHdhaXQoKSB1bnRpbCBDaGFyOkZpbmRGaXJzdENoaWxkKCJIdW1hbm9pZCIp
-bG9jYWwgSHVtID0gQ2hhci5IdW1hbm9pZA
-SHVtLkNoYW5nZWQ6Y29ubmVjdChmdW5jdGlvbigp
-SHVtLldhbGtTcGVlZCA9IF9HLkhhY2tlZFdhbGtTcGVlZA
-ZW5kKQ
-SHVtLldhbGtTcGVlZCA9IF9HLkhhY2tlZFdhbGtTcGVlZA
-ZW5kKQ
-ICBlbmQgICAg
-fSk
-ICA
-ICA
-ICA
-ICA
-ICA
-ZW5k
-T3Jpb25MaWI6SW5pdCgp
+if game.PlaceId == 6490016198 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "UpDATE🧊Slayer Tycoon (v1.5) | CreozilHub", HidePremium = false, IntroText = "CreozilHub", SaveConfig = true, ConfigFolder = "CreozilHub"})
+  
+--Value
+  
+  
+--Functions
+  
+  
+--Tabs
+local MainTab = Window:MakeTab({
+  Name = "Main",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
+})
+  
+--Toggles  
+MainTab:AddSlider({
+  Name = "SpeedHack",
+  Min = 0,
+  Max = 200,
+  Default = 16,
+  Color = Color3.fromRGB(255,255,255),
+  Increment = 1,
+  ValueName = "",
+  Callback = function(s)  
+_G.HackedWalkSpeed = s
+  
+local Plrs = game:GetService("Players")
+  
+local MyPlr = Plrs.LocalPlayer
+local MyChar = MyPlr.Character
+  
+if MyChar then
+local Hum = MyChar.Humanoid
+Hum.Changed:connect(function(s)
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end
+  
+  
+MyPlr.CharacterAdded:connect(function(Char)
+MyChar = Char
+repeat wait() until Char:FindFirstChild("Humanoid")
+local Hum = Char.Humanoid
+Hum.Changed:connect(function()
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
+  end    
+})
+  
+  
+  
+  
+  
+end
+OrionLib:Init()
