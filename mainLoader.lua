10b715f19d4744e4303c898da7507451ef0f44b6
jmxtrex <121318872+jmxtrex@users.noreply.github.com>
2022-12-29 04:16:36 +0300

    Update and rename mainLoader to mainLoader.lua

---
+++ mainLoader.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 6490016198 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+
+end
+OrionLib:Init()
