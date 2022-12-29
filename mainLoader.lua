c930a8164da8b0507e534084cf7df671de6e1a74
jmxtrex <121318872+jmxtrex@users.noreply.github.com>
2022-12-29 04:21:35 +0300

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -1,8 +1,1 @@
-if game.PlaceId == 6490016198 then
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "UpDATE🧊Slayer Tycoon (v1.5) | CreozilHub", HidePremium = false, IntroText = "CreozilHub", SaveConfig = true, ConfigFolder = "CreozilHub"})
 
-
-
-end
-OrionLib:Init()
